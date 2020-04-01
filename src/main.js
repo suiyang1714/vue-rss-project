db48c853806df6fc4da816ef47cb069c2d52579c
Aditya <171426589@qq.com>
2020-04-01 15:15:44 +0800

    Add 订阅源界面、详情、自定义

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Vuex from 'vuex'
+import router from './router/router'
+import store from './store'
+import 'amfe-flexible'
+import moment from 'dayjs'
+import 'vant/lib/toast/style';
+import { Toast } from 'vant'
+
+require('./assets/base.css')
+const relativeTime = require('dayjs/plugin/relativeTime')
+
+Vue.use(Vuex)
+Vue.config.productionTip = false
+Vue.use(Toast)
+moment.extend(relativeTime)
+Vue.prototype.$moment = moment;
+
+new Vue({
+  store,
+  router,
+  render: h => h(App),
+}).$mount('#app')
